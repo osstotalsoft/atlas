47b1dd7636a979a630f349a225893c00f636f01a
Elena.Dumitrescu@totalsoft.ro <elena.dumitrescu@totalsoft.ro>
2022-05-06 10:01:06 +0300

    Added warning on missing task or subworkflow

--- react-ui/src/features/common/components/modals/WorkflowPresentation.js
+++ react-ui/src/features/common/components/modals/WorkflowPresentation.js
@@ -6,8 +6,11 @@ import LoadingFakeText from 'components/LoadingFakeText'
 import { WORKFLOW_QUERY } from 'features/workflow/edit/queries/WorkflowQuery'
 import { emptyObject } from 'utils/constants'
 import { useQuery } from '@apollo/client'
+import { Typography } from '@bit/totalsoft_oss.react-mui.kit.core'
+import { useTranslation } from 'react-i18next'
 
 const WorkflowPresentation = ({ name, version }) => {
+  const { t } = useTranslation()
   const [diagram] = useState(getApplicationDiagram())
   const { engine } = diagram
 
@@ -15,9 +18,15 @@ const WorkflowPresentation = ({ name, version }) => {
     variables: { name, version, skip: false }
   })
 
+  const workflow = data?.getWorkflow
+
   if (loading) return <LoadingFakeText lines={8} />
 
-  return <BodyWidget canvasClass={'dataflow-canvas-popover'} workflow={data?.getWorkflow || emptyObject} engine={engine} locked={true} />
+  return workflow ? (
+    <BodyWidget canvasClass={'dataflow-canvas-popover'} workflow={workflow || emptyObject} engine={engine} locked={true} />
+  ) : (
+    <Typography variant='h6'>{t('Workflow.WorkflowNotFound')}</Typography>
+  )
 }
 
 WorkflowPresentation.propTypes = {
