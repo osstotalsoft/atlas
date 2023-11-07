91f01751d17954c89838b754b87e205548334571
gpalade <gpalade@totalsoft.ro>
2023-11-07 13:42:59 +0200

    fix owner email

--- react-ui/src/utils/functions.js
+++ react-ui/src/utils/functions.js
@@ -111,3 +111,8 @@ export const sortBy = curry((field, direction, array) => {
       return sortWith([descend(prop(field))], array)
   }
 })
+
+export const validateEmail = (email) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)+)|([^<>()[\]\\.,;:\s@"]{2,})|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]{2,}\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase())
+};
